a19c37d0c128ce11974c26751110e6746316cb5c
SergeiSko <34917913+SergeiSko@users.noreply.github.com>
2018-07-23 12:45:02 +0300

    QWE

--- config/database.js
+++ config/database.js
@@ -6,21 +6,8 @@ module.exports = {
         'password': '',
         'charset': "utf8_bin"
     },
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> bd04178b66d3335302275b4453d454445a98163e
-	'database': 'kartli',
-    'users_table': 'users'
-=======
 	'database': 'kartlibeta',
     'users_table': 'users',
     'usertype_table' : 'usertype'
->>>>>>> fd24adaea7a64abba07097c71a77936c513f1866
-=======
-	'database': 'kartlibeta',
-    'users_table': 'users',
-    'usertype_table' : 'usertype'
->>>>>>> fd24adaea7a64abba07097c71a77936c513f1866
+
 };
